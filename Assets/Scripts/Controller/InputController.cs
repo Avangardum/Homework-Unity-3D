310e3db062d80613745e9a70b610543ee7264c10
Юрий Кузьминых <yurykuz65@gmail.com>
2020-01-19 23:07:47 +0300

    Перенёс некоторые скрипты из исходников

--- Assets/Scripts/Controller/InputController.cs
+++ Assets/Scripts/Controller/InputController.cs
@@ -5,12 +5,19 @@ namespace Geekbrains
     public sealed class InputController : BaseController, IExecute
     {
         private KeyCode _activeFlashLight = KeyCode.F;
+        private int _shootMouseButton = 0;
+
         public void Execute()
         {
             if (!IsActive) return;
             if (Input.GetKeyDown(_activeFlashLight))
             {
                 ServiceLocator.Resolve<FlashLightController>().Switch();
+            }
+
+            if (Input.GetMouseButtonDown(_shootMouseButton))
+            {
+                
             }
         }
     }
